12d2e4bfa221fc67a5d434d1cfa11a09ea217b13
SpI3dt <siddhab27@gmail.com>
2026-04-10 08:16:31 +0530

    WEEK3-4 Level 1

---
+++ level1/LargestCheck.java
@@ -0,0 +1,27 @@
+/**
+ * Level 1 C
+ * To check if a person is elligible to vote or not
+ */
+
+
+
+public class LargestCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int n1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int n2 = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int n3 = sc.nextInt();
+
+        System.out.println("Is the first number the largest? " + (n1 > n2 && n1 > n3 ? "Yes" : "No"));
+        System.out.println("Is the second number the largest? " + (n2 > n1 && n2 > n3 ? "Yes" : "No"));
+        System.out.println("Is the third number the largest? " + (n3 > n1 && n3 > n2 ? "Yes" : "No"));
+
+        sc.close();
+    }
+}
